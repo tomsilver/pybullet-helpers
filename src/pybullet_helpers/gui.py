973ae5fc7493491fa78145db65ce633fad416849
Tom Silver <tomssilver@gmail.com>
2024-08-08 09:45:28 -0400

    make it easier for external robots to use gui tool

---
+++ src/pybullet_helpers/gui.py
@@ -0,0 +1,42 @@
+"""Utilities for GUIs."""
+
+import numpy as np
+import pybullet as p
+
+from pybullet_helpers.robots.single_arm import SingleArmPyBulletRobot
+
+
+def run_interactive_joint_gui(robot: SingleArmPyBulletRobot) -> None:
+    """Visualize a robot's joint space."""
+    initial_joints = robot.get_joint_positions()
+
+    slider_ids: list[int] = []
+    for i, joint_name in enumerate(robot.arm_joint_names):
+        lower, upper = robot.get_joint_limits_from_name(joint_name)
+        # Handle circular joints.
+        if np.isinf(lower):
+            lower = -10
+        if np.isinf(upper):
+            upper = 10
+        current = initial_joints[i]
+        slider_id = p.addUserDebugParameter(
+            paramName=joint_name,
+            rangeMin=lower,
+            rangeMax=upper,
+            startValue=current,
+            physicsClientId=robot.physics_client_id,
+        )
+        slider_ids.append(slider_id)
+
+    p.setRealTimeSimulation(True, physicsClientId=robot.physics_client_id)
+    while True:
+        joint_positions = []
+        for slider_id in slider_ids:
+            try:
+                v = p.readUserDebugParameter(
+                    slider_id, physicsClientId=robot.physics_client_id
+                )
+            except p.error:
+                print("WARNING: failed to read parameter, skipping")
+            joint_positions.append(v)
+        robot.set_joints(joint_positions)
