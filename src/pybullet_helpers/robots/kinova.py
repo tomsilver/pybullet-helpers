bfcbffcc0f3a3a27e23e164f5289697840417c7a
Tom Silver <tomsilver@users.noreply.github.com>
2024-08-08 11:14:31 -0400

    add kinova gen3 robot with robotiq gripper (#7)

--- src/pybullet_helpers/robots/kinova.py
+++ src/pybullet_helpers/robots/kinova.py
@@ -3,7 +3,10 @@
 from pathlib import Path
 
 from pybullet_helpers.joint import JointPositions
-from pybullet_helpers.robots.single_arm import SingleArmPyBulletRobot
+from pybullet_helpers.robots.single_arm import (
+    SingleArmPyBulletRobot,
+    SingleArmTwoFingerGripperPyBulletRobot,
+)
 from pybullet_helpers.utils import get_assets_path
 
 
@@ -17,7 +20,7 @@ class KinovaGen3NoGripperPyBulletRobot(SingleArmPyBulletRobot):
     @classmethod
     def urdf_path(cls) -> Path:
         dir_path = get_assets_path() / "urdf"
-        return dir_path / "kinova_description" / "GEN3_URDF_V12.urdf"
+        return dir_path / "kinova_no_gripper" / "GEN3_URDF_V12.urdf"
 
     @property
     def default_home_joint_positions(self) -> JointPositions:
@@ -30,3 +33,44 @@ class KinovaGen3NoGripperPyBulletRobot(SingleArmPyBulletRobot):
     @property
     def tool_link_name(self) -> str:
         return "EndEffector_Link"
+
+
+class KinovaGen3RobotiqGripperPyBulletRobot(SingleArmTwoFingerGripperPyBulletRobot):
+    """A Kinova Gen3 robot arm with a robotiq gripper."""
+
+    @classmethod
+    def get_name(cls) -> str:
+        return "kinova-gen3"
+
+    @classmethod
+    def urdf_path(cls) -> Path:
+        dir_path = get_assets_path() / "urdf"
+        return dir_path / "kortex_description" / "gen3_robotiq_2f_85.urdf"
+
+    @property
+    def default_home_joint_positions(self) -> JointPositions:
+        return [-4.3, -1.6, -4.8, -1.8, -1.4, -1.1, 1.6, 0.0, 0.0]
+
+    @property
+    def end_effector_name(self) -> str:
+        return "end_effector"
+
+    @property
+    def tool_link_name(self) -> str:
+        return "end_effector_link"
+
+    @property
+    def left_finger_joint_name(self) -> str:
+        return "left_inner_finger_joint"
+
+    @property
+    def right_finger_joint_name(self) -> str:
+        return "right_inner_finger_joint"
+
+    @property
+    def open_fingers(self) -> float:
+        return 0.5
+
+    @property
+    def closed_fingers(self) -> float:
+        return -0.5
