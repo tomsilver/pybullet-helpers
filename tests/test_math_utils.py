c9462af9fadf816b1216ca9adb95a2568670f3ca
Tom Silver <tomsilver@users.noreply.github.com>
2024-08-11 14:45:51 -0400

    add angle_offset kwarg to get_poses_facing_line (#17)

--- tests/test_math_utils.py
+++ tests/test_math_utils.py
@@ -1,5 +1,7 @@
 """Tests for math_utils.py."""
 
+import numpy as np
+
 from pybullet_helpers.geometry import Pose
 from pybullet_helpers.math_utils import get_poses_facing_line
 
@@ -46,9 +48,24 @@ def test_get_poses_facing_line():
     for p, e in zip(poses, expected, strict=True):
         assert p.allclose(e)
 
+    # Test angle_offset.
+    axis = (0.0, 0.0, 1.0)
+    point_on_line = (0.0, 0.0, 0.0)
+    radius = 1.0
+    poses = get_poses_facing_line(
+        axis, point_on_line, radius, num_points=4, angle_offset=np.pi / 2
+    )
+    expected = [
+        Pose(position=(0.0, 1.0, 0.0), orientation=(0.0, 0.707107, -0.707107, 0.0)),
+        Pose(position=(-1.0, 0.0, 0.0), orientation=(-0.5, 0.5, -0.5, 0.5)),
+        Pose(position=(0.0, -1.0, 0.0), orientation=(-0.707107, 0.0, 0.0, 0.707107)),
+        Pose(position=(1.0, 0.0, 0.0), orientation=(-0.5, -0.5, 0.5, 0.5)),
+    ]
+    for p, e in zip(poses, expected, strict=True):
+        assert p.allclose(e)
+
     # Uncomment to debug.
     # import pybullet as p
-    # import numpy as np
     # from pybullet_helpers.gui import create_gui_connection, visualize_pose
 
     # physics_client_id = create_gui_connection(camera_target=point_on_line)
