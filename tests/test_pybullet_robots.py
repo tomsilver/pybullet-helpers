59add75fcb4fe4637d1b077f4a9963d9651dd2d8
Tom Silver <tomssilver@gmail.com>
2024-08-11 10:20:27 -0400

    wip

--- tests/test_pybullet_robots.py
+++ tests/test_pybullet_robots.py
@@ -73,69 +73,6 @@ def _setup_pybullet_test_scene():
     p.disconnect(physics_client_id)
 
 
-def test_fetch_pybullet_robot(physics_client_id):
-    """Tests for FetchPyBulletRobot()."""
-    base_pose = Pose((0.75, 0.7441, 0.0))
-    robot = FetchPyBulletRobot(
-        physics_client_id,
-        base_pose=base_pose,
-    )
-    assert robot.get_name() == "fetch"
-    assert robot.arm_joint_names == [
-        "shoulder_pan_joint",
-        "shoulder_lift_joint",
-        "upperarm_roll_joint",
-        "elbow_flex_joint",
-        "forearm_roll_joint",
-        "wrist_flex_joint",
-        "wrist_roll_joint",
-        "l_gripper_finger_joint",
-        "r_gripper_finger_joint",
-    ]
-    assert np.allclose(robot.action_space.low, robot.joint_lower_limits)
-    assert np.allclose(robot.action_space.high, robot.joint_upper_limits)
-    # The robot arm is 7 DOF and the left and right fingers are appended last.
-    assert robot.left_finger_joint_idx == 7
-    assert robot.right_finger_joint_idx == 8
-
-    ee_target_position = (1.34, 0.75, 0.76)
-    ee_orn = p.getQuaternionFromEuler([0.0, np.pi / 2, -np.pi])
-    ee_target = Pose(ee_target_position, ee_orn)
-    joint_target = inverse_kinematics(robot, ee_target, validate=False)
-    f_value = 0.03
-    joint_target[robot.left_finger_joint_idx] = f_value
-    joint_target[robot.right_finger_joint_idx] = f_value
-    action_arr = np.array(joint_target, dtype=np.float32)
-
-    # Not a valid control mode.
-    robot._control_mode = "not a real control mode"  # pylint: disable=protected-access
-    with pytest.raises(NotImplementedError) as e:
-        robot.set_motors(action_arr)
-    assert "Unrecognized pybullet_control_mode" in str(e)
-
-    # Reset control mode.
-    robot._control_mode = "reset"  # pylint: disable=protected-access
-    robot.set_motors(action_arr)  # just make sure it doesn't crash
-
-    # Position control mode.
-    robot._pybullet_control_mode = "position"  # pylint: disable=protected-access
-    robot.set_motors(action_arr)
-    for _ in range(20):
-        p.stepSimulation(physicsClientId=physics_client_id)
-    recovered_ee_pos = robot.get_end_effector_pose().position
-
-    # IK is currently not precise enough to increase this tolerance.
-    assert np.allclose(ee_target_position, recovered_ee_pos, atol=1e-2)
-    # Test forward kinematics.
-    fk_result = robot.forward_kinematics(action_arr)
-    assert np.allclose(fk_result.position, ee_target_position, atol=1e-2)
-
-    # Check link_from_name
-    assert robot.link_from_name("gripper_link")
-    with pytest.raises(ValueError):
-        robot.link_from_name("non_existent_link")
-
-
 def test_kinova_gen3_no_gripper_pybullet_robot(physics_client_id):
     """Tests for KinovaGen3NoGripperPyBulletRobot()."""
     base_pose = Pose((0.75, 0.7441, 0.0))
