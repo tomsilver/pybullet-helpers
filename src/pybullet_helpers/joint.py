df8d277f1543128815666938e08afb62f1db2fdf
Eric Hu <56524795+ericlhu0@users.noreply.github.com>
2024-08-16 18:45:07 -0400

    fix get_joint_states for robots with no free joints (#34)

--- src/pybullet_helpers/joint.py
+++ src/pybullet_helpers/joint.py
@@ -164,12 +164,17 @@ def get_joint_states(
     body: int, joints: list[int], physics_client_id: int
 ) -> list[JointState]:
     """Get the joint states for the given joints for a body."""
-    joint_states = [
-        JointState(*raw_joint_state)
-        for raw_joint_state in p.getJointStates(
-            body, joints, physicsClientId=physics_client_id
-        )
-    ]
+
+    if len(joints) == 0:
+        joint_states = []
+    else:
+        joint_states = [
+            JointState(*raw_joint_state)
+            for raw_joint_state in p.getJointStates(
+                body, joints, physicsClientId=physics_client_id
+            )
+        ]
+
     return joint_states
 
 
