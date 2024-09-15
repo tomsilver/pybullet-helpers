e945513d6424a93a25f4a7f582d9d252b07536d9
Tom Silver <tomsilver@users.noreply.github.com>
2024-09-15 10:57:55 -0400

    add stretch robot with sg3 gripper (relatively untested) (#53)

---
+++ tests/robots/test_stretch.py
@@ -0,0 +1,11 @@
+"""Tests for Stretch robot."""
+
+from pybullet_helpers.robots.stretch import StretchPyBulletRobot
+
+
+def test_stretch_pybullet_robot(physics_client_id):
+    """Tests for StretchPyBulletRobot()."""
+    robot = StretchPyBulletRobot(physics_client_id)
+    assert robot.get_name() == "stretch"
+    assert not robot.fixed_base
+    assert len(robot.default_home_joint_positions) == 10
