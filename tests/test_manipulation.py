13cdacfede47e86232aaa8d92d09aa459e8cd3f0
Tom Silver <tomsilver@users.noreply.github.com>
2024-10-15 11:48:59 -0400

    implement place skill function  (#65)

--- tests/test_manipulation.py
+++ tests/test_manipulation.py
@@ -4,17 +4,23 @@ import numpy as np
 import pybullet as p
 
 from pybullet_helpers.geometry import Pose, multiply_poses, set_pose
-from pybullet_helpers.manipulation import get_kinematic_plan_to_pick_object
+from pybullet_helpers.manipulation import (
+    get_kinematic_plan_to_pick_object,
+    get_kinematic_plan_to_place_object,
+)
 from pybullet_helpers.math_utils import get_poses_facing_line
 from pybullet_helpers.robots import create_pybullet_robot
 from pybullet_helpers.states import KinematicState
 from pybullet_helpers.utils import create_pybullet_block, create_pybullet_cylinder
 
 
-def test_get_kinematic_plan_to_pick_object():
-    """Tests for get_kinematic_plan_to_pick_object()."""
+def test_kinematic_pick_place():
+    """Tests for get_kinematic_plan_to_pick_object() and
+    get_kinematic_plan_to_place_object."""
 
-    # Set up a scene to test picking.
+    rng = np.random.default_rng(123)
+
+    # Set up a scene to test manipuation.
     physics_client_id = p.connect(p.DIRECT)
 
     # Uncomment to debug.
@@ -52,20 +58,18 @@ def test_get_kinematic_plan_to_pick_object():
     initial_state = KinematicState.from_pybullet(robot, {object_id, table_id})
 
     # Set up a grasp generator.
-    rng = np.random.default_rng(5)
-
     def _grasp_generator():
         while True:
             angle_offset = rng.uniform(-np.pi, np.pi)
             relative_pose = get_poses_facing_line(
                 axis=(0.0, 0.0, 1.0),
                 point_on_line=(0.0, 0.0, 0.0),
-                radius=0.5 * object_radius,
+                radius=0.0125,
                 num_points=1,
                 angle_offset=angle_offset,
             )[0]
             rot = Pose.from_rpy((0, 0, 0), (0.0, 0.0, np.pi / 2))
-            yield multiply_poses(object_pose, relative_pose, rot)
+            yield multiply_poses(relative_pose, rot)
 
     # Get a plan.
     plan = get_kinematic_plan_to_pick_object(
@@ -78,3 +82,38 @@ def test_get_kinematic_plan_to_pick_object():
     )
 
     assert plan is not None
+
+    # Advance to the end of the plan.
+    initial_state = plan[-1]
+    initial_state.set_pybullet(robot)
+
+    # Set up a placement generator.
+    placement_lb = (
+        -table_half_extents[0] + object_radius,
+        -table_half_extents[1] + object_radius,
+        table_half_extents[2] + object_length / 2,
+    )
+    placement_ub = (
+        table_half_extents[0] - object_radius,
+        table_half_extents[1] - object_radius,
+        table_half_extents[2] + object_length / 2,
+    )
+
+    def _placement_generator():
+        # Sample on the surface of the table.
+        while True:
+            yield Pose(tuple(rng.uniform(placement_lb, placement_ub)))
+
+    # Get a plan.
+    plan = get_kinematic_plan_to_place_object(
+        initial_state,
+        robot,
+        object_id,
+        table_id,
+        collision_ids={table_id},
+        placement_generator=_placement_generator(),
+    )
+
+    assert plan is not None
+
+    p.disconnect(physics_client_id)
