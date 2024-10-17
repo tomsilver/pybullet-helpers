9054d28e3e1d26921f07e88629a44d15771b77f8
Tom Silver <tomsilver@users.noreply.github.com>
2024-10-17 01:31:18 -0400

    add robot base to kinematic state (#67)

--- src/pybullet_helpers/states.py
+++ src/pybullet_helpers/states.py
@@ -18,6 +18,8 @@ class KinematicState:
     object_poses: dict[int, Pose]  # maps pybullet ID to pose
     # Maps held object IDs to end-effector transforms.
     attachments: dict[int, Pose] = field(default_factory=dict)
+    # Only need to track base for non-fixed-base robots.
+    robot_base_pose: Pose | None = None
 
     @classmethod
     def from_pybullet(
@@ -28,6 +30,7 @@ class KinematicState:
     ) -> KinematicState:
         """Create a KinematicStatic from pybullet."""
         robot_joints = robot.get_joint_positions()
+        robot_base_pose = None if robot.fixed_base else robot.get_base_pose()
         object_poses = {o: get_pose(o, robot.physics_client_id) for o in object_ids}
         attachments: dict[int, Pose] = {}
         if attached_object_ids is not None:
@@ -42,11 +45,13 @@ class KinematicState:
                     world_to_ee_link.invert(), world_to_object
                 )
                 attachments[object_id] = ee_link_to_object
-        return KinematicState(robot_joints, object_poses, attachments)
+        return KinematicState(robot_joints, object_poses, attachments, robot_base_pose)
 
     def set_pybullet(self, robot: SingleArmPyBulletRobot) -> None:
         """Reset the pybullet simulator to this kinematic state."""
         robot.set_joints(self.robot_joints)
+        if self.robot_base_pose is not None:
+            robot.set_base(self.robot_base_pose)
         for object_id, pose in self.object_poses.items():
             set_pose(object_id, pose, robot.physics_client_id)
         world_to_ee_link = get_link_state(
@@ -58,10 +63,17 @@ class KinematicState:
             world_to_object = multiply_poses(world_to_ee_link, ee_link_to_object)
             set_pose(object_id, world_to_object, robot.physics_client_id)
 
-    def copy_with(self, robot_joints: JointPositions) -> KinematicState:
+    def copy_with(
+        self,
+        robot_joints: JointPositions | None = None,
+        robot_base_pose: Pose | None = None,
+    ) -> KinematicState:
         """Create a copy of this state with replacements."""
         # For now, only robot joint copying is needed, but this function can
         # be extended in the future if needed.
         return KinematicState(
-            robot_joints, self.object_poses.copy(), self.attachments.copy()
+            (robot_joints or self.robot_joints),
+            self.object_poses.copy(),
+            self.attachments.copy(),
+            (robot_base_pose or self.robot_base_pose),
         )
