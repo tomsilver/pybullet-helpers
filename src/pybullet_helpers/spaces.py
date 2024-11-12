62bfda61a379a47f3f9a222c041d0ef60421b04c
Tom Silver <tomssilver@gmail.com>
2024-11-12 17:00:35 -0500

    wip

--- src/pybullet_helpers/spaces.py
+++ src/pybullet_helpers/spaces.py
@@ -34,52 +34,59 @@ class PoseSpace(gym.spaces.Space[Pose]):
         seed: int | np.random.Generator | None = None,
     ) -> None:
         super().__init__(shape=None, dtype=None, seed=seed)
-        self._x_min = x_min
-        self._x_max = x_max
-        self._y_min = y_min
-        self._y_max = y_max
-        self._z_min = z_min
-        self._z_max = z_max
-        self._roll_min = roll_min
-        self._roll_max = roll_max
-        self._pitch_min = pitch_min
-        self._pitch_max = pitch_max
-        self._yaw_min = yaw_min
-        self._yaw_max = yaw_max
-        self._all_bounds = [
-            [self._x_min, self._x_max],
-            [self._y_min, self._y_max],
-            [self._z_min, self._z_max],
-            [self._roll_min, self._roll_max],
-            [self._pitch_min, self._pitch_max],
-            [self._yaw_min, self._yaw_max],
+        self._x_min, self._x_max = x_min, x_max
+        self._y_min, self._y_max = y_min, y_max
+        self._z_min, self._z_max = z_min, z_max
+        self._roll_min, self._roll_max = roll_min, roll_max
+        self._pitch_min, self._pitch_max = pitch_min, pitch_max
+        self._yaw_min, self._yaw_max = yaw_min, yaw_max
+        self._min = [
+            self._x_min,
+            self._y_min,
+            self._z_min,
+            self._roll_min,
+            self._pitch_min,
+            self._yaw_min,
+        ]
+        self._max = [
+            self._x_max,
+            self._y_max,
+            self._z_max,
+            self._roll_max,
+            self._pitch_max,
+            self._yaw_max,
         ]
 
     def sample(self, mask: Any | None = None) -> Pose:
         # See docstring note!
-        x, y, z, roll, pitch, yaw = self.np_random.uniform(self._all_bounds)
+        x, y, z, roll, pitch, yaw = self.np_random.uniform(self._min, self._max)
         return Pose.from_rpy((x, y, z), (roll, pitch, yaw))
 
     def contains(self, x: Any) -> bool:
         if not isinstance(x, Pose):
             return False
-        if not self._x_min <= x.position[0] <= self._x_max:
-            return False
-        if not self._y_min <= x.position[1] <= self._y_max:
-            return False
-        if not self._z_min <= x.position[2] <= self._z_max:
-            return False
-        if not self._roll_min <= x.rpy[0] <= self._roll_max:
-            return False
-        if not self._pitch_min <= x.rpy[1] <= self._pitch_max:
-            return False
-        if not self._yaw_min <= x.rpy[2] <= self._yaw_max:
-            return False
-        return True
+        pos_in_bounds = (
+            self._x_min <= x.position[0] <= self._x_max
+            and self._y_min <= x.position[1] <= self._y_max
+            and self._z_min <= x.position[2] <= self._z_max
+        )
+        rpy_in_bounds = (
+            self._roll_min <= x.rpy[0] <= self._roll_max
+            and self._pitch_min <= x.rpy[1] <= self._pitch_max
+            and self._yaw_min <= x.rpy[2] <= self._yaw_max
+        )
+        return pos_in_bounds and rpy_in_bounds
 
     @property
     def is_np_flattenable(self) -> bool:
         return False
 
     def __repr__(self) -> str:
-        return f"PoseSpace()"
+        return (
+            f"PoseSpace(x_range=({self._x_min}, {self._x_max}), "
+            f"y_range=({self._y_min}, {self._y_max}), "
+            f"z_range=({self._z_min}, {self._z_max}), "
+            f"roll_range=({self._roll_min}, {self._roll_max}), "
+            f"pitch_range=({self._pitch_min}, {self._pitch_max}), "
+            f"yaw_range=({self._yaw_min}, {self._yaw_max}))"
+        )
