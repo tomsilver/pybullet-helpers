1a1745aa11133a506556926fd1c7240d5d01d466
Tom Silver <tomssilver@gmail.com>
2025-03-29 10:03:14 -0400

    wip

--- tests/robots/test_human.py
+++ tests/robots/test_human.py
@@ -1,17 +1,15 @@
 """Tests for Human()."""
 
-from pybullet_helpers.robots.human import Human
-from pybullet_helpers.geometry import Pose
 import numpy as np
+
+from pybullet_helpers.geometry import Pose
+from pybullet_helpers.inverse_kinematics import inverse_kinematics
 from pybullet_helpers.math_utils import sample_within_sphere
+from pybullet_helpers.robots.human import Human
 
 
 def test_human(physics_client_id):
     """Tests for Human()."""
-
-    # TODO remove
-    from pybullet_helpers.gui import create_gui_connection
-    physics_client_id = create_gui_connection()
 
     right_leg_kwargs = {
         "home_joint_positions": [-np.pi / 2, 0.0, 0.0, 0.0, 0.0, 0.0],
@@ -21,9 +19,13 @@ def test_human(physics_client_id):
     }
 
     base_pose = Pose.from_rpy((0.1, 0.0, 0.0), (0.0, 0.0, np.pi / 2))
-    human = Human(physics_client_id, base_pose=base_pose, right_leg_kwargs=right_leg_kwargs,
-                  left_leg_kwargs=left_leg_kwargs)
-    
+    human = Human(
+        physics_client_id,
+        base_pose=base_pose,
+        right_leg_kwargs=right_leg_kwargs,
+        left_leg_kwargs=left_leg_kwargs,
+    )
+
     # Test IK for the right arm.
     right_arm = human.right_arm
 
@@ -32,17 +34,11 @@ def test_human(physics_client_id):
     resting_pose = right_arm.get_end_effector_pose()
     center = resting_pose.position
     orientation = resting_pose.orientation
-    radius = 0.001
+    radius = 0.05
     rng = np.random.default_rng(123)
-    for _ in range(10):
+    for _ in range(5):
         position = sample_within_sphere(center, 0.0, radius, rng)
         pose = Pose(position, orientation)
-
-        # TODO remove
-        from pybullet_helpers.gui import visualize_pose
-        visualize_pose(pose, physics_client_id)
-
-
-    import pybullet as p
-    while True:
-        p.getMouseEvents(human.physics_client_id)
+        joints = inverse_kinematics(right_arm, pose)
+        right_arm.set_joints(joints)
+        assert right_arm.get_end_effector_pose().allclose(pose, atol=1e-3)
