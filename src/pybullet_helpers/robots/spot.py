e2f1a61f1327fcc4fe9b41fc322fc866b3eea0ce
Tom Silver <tomsilver@users.noreply.github.com>
2025-08-16 10:48:49 -0400

    add analytic spot IK (#122)

--- src/pybullet_helpers/robots/spot.py
+++ src/pybullet_helpers/robots/spot.py
@@ -3,7 +3,9 @@
 from pathlib import Path
 
 import numpy as np
+import numpy.typing as npt
 
+from pybullet_helpers.geometry import Pose, multiply_poses
 from pybullet_helpers.joint import JointPositions
 from pybullet_helpers.robots.single_arm import (
     FingeredSingleArmPyBulletRobot,
@@ -64,3 +66,131 @@ class SpotPyBulletRobot(FingeredSingleArmPyBulletRobot):
         assert len(joint_positions) == 1
         finger_state = joint_positions[0]
         return finger_state
+
+    @property
+    def default_inverse_kinematics_method(self) -> str:
+        return "custom"
+
+    def custom_inverse_kinematics(
+        self,
+        end_effector_pose: Pose,
+        validate: bool = True,
+        best_effort: bool = False,
+        validation_atol: float = 1e-3,
+    ) -> JointPositions:
+        """Analytic IK for 6-DOF spot arm.
+
+        I believe the original author of this is Tomas Lozano-Perez.
+        """
+        # The target end_effector_pose is in the world frame. Convert it into the
+        # robot shoulder frame.
+        world_to_base = self.get_base_pose()
+        world_to_shoulder = multiply_poses(world_to_base, Pose((0.292, 0.0, 0.873)))
+        shoulder_to_target = multiply_poses(
+            world_to_shoulder.invert(), end_effector_pose
+        )
+        solns = _analytic_spot_ik_6(
+            shoulder_to_target.to_matrix(),
+            self.joint_lower_limits[:6],
+            self.joint_upper_limits[:6],
+        )
+        # Arbitrarily select the first one. Later, might want to use distances.
+        assert solns
+        return list(solns[0]) + [self.get_finger_state()]
+
+
+def _analytic_spot_ik_6(
+    wrist_pose_rel_shoulder: npt.NDArray,
+    min_limits: JointPositions,
+    max_limits: JointPositions,
+) -> list[JointPositions]:
+    px, py, pz = wrist_pose_rel_shoulder[:3, 3]  # wrist position
+    l2 = 0.3385
+    l3 = np.sqrt(0.40330**2 + 0.0750**2)
+    q3_off = np.arctan2(0.0750, 0.40330)
+    # Solve the first three joints based on position.
+    q123_sols = []
+    xl = np.sqrt(px**2 + py**2)
+    q23 = _IK2R(l2, l3, xl, -pz)
+    if q23 is None:
+        return []
+    q1 = np.arctan2(py, px)
+    for q2, q3 in q23:
+        q123_sols.append((q1, q2, q3 + q3_off))
+    q23 = _IK2R(l2, l3, -xl, -pz)
+    assert q23 is not None
+    q1 = q1 + np.pi
+    for q2, q3 in q23:
+        q123_sols.append((q1, q2, q3 + q3_off))
+
+    # Solve for the wrist angles.
+    qfull_sols = []
+    for q1, q2, q3 in q123_sols:
+        r3_inv = np.linalg.inv(
+            _rotation_matrix(q1, (0, 0, 1)) @ _rotation_matrix(q2 + q3, (0, 1, 0))
+        )
+        W = r3_inv @ wrist_pose_rel_shoulder
+        q5 = np.arccos(W[0, 0])
+        for q in (q5, -q5):
+            s5 = np.sin(q)
+            q4 = np.arctan2(W[1, 0] / s5, -W[2, 0] / s5)
+            q6 = np.arctan2(W[0, 1] / s5, W[0, 2] / s5)
+            angles = (q1, q2, q3, q4, q, q6)
+            if (
+                np.less_equal(min_limits, angles).all()
+                and np.less_equal(angles, max_limits).all()
+            ):
+                qfull_sols.append(list(angles))
+    return qfull_sols
+
+
+def _IK2R(L1: float, L2: float, x: float, y: float) -> list[tuple[float, float]] | None:
+    xy_sq = x**2 + y**2
+    c2 = (xy_sq - L1**2 - L2**2) / (2 * L1 * L2)
+    if abs(c2) > 1:
+        return None
+    if c2 == 1.0:
+        return [(np.arctan2(y, x), 0)]
+    if c2 == -1.0 and xy_sq != 0.0:
+        return [(np.arctan2(y, x), np.pi)]
+    if c2 == -1.0 and xy_sq == 0.0:
+        return [(q1, np.pi) for q1 in [0, 2 * np.pi]]
+    q2_1 = np.arccos(c2)
+    q2_2 = -q2_1
+    theta = np.arctan2(y, x)
+    q1q2 = [
+        (theta - np.arctan2(L2 * np.sin(q2_i), L1 + L2 * np.cos(q2_i)), q2_i)
+        for q2_i in (q2_1, q2_2)
+    ]
+    for q1, q2 in q1q2:
+        xk = L1 * np.cos(q1) + L2 * np.cos(q1 + q2)
+        yk = L1 * np.sin(q1) + L2 * np.sin(q1 + q2)
+        assert abs(x - xk) < 0.0001
+        assert abs(y - yk) < 0.0001
+    return q1q2
+
+
+def _rotation_matrix(
+    angle: float, direction: tuple[float, float, float]
+) -> npt.NDArray:
+    """Return matrix to rotate about axis defined by point and direction."""
+    sina = np.sin(angle)
+    cosa = np.cos(angle)
+    direction = direction[:3] / np.linalg.norm(direction[:3])  # type: ignore
+    # Rotation matrix around unit vector.
+    R = np.array(
+        ((cosa, 0.0, 0.0), (0.0, cosa, 0.0), (0.0, 0.0, cosa)), dtype=np.float64
+    )
+    R += np.outer(direction, direction) * (1.0 - cosa)
+    direction *= sina
+    R += np.array(
+        (
+            (0.0, -direction[2], direction[1]),
+            (direction[2], 0.0, -direction[0]),
+            (-direction[1], direction[0], 0.0),
+        ),
+        dtype=np.float64,
+    )
+    M = np.identity(4)
+    M[:3, :3] = R
+    return M
--- tests/robots/test_spot.py
+++ tests/robots/test_spot.py
@@ -2,6 +2,8 @@
 
 import numpy as np
 
+from pybullet_helpers.geometry import Pose, multiply_poses
+from pybullet_helpers.inverse_kinematics import inverse_kinematics
 from pybullet_helpers.robots.spot import (
     SpotPyBulletRobot,
 )
@@ -11,6 +13,7 @@ def test_spot_pybullet_robot(physics_client_id):
     """Tests for SpotPyBulletRobot()."""
     robot = SpotPyBulletRobot(
         physics_client_id,
+        fixed_base=False,
     )
     assert robot.get_name() == "spot"
     assert robot.arm_joint_names == [
@@ -24,3 +27,55 @@ def test_spot_pybullet_robot(physics_client_id):
     ]
     assert np.allclose(robot.action_space.low, robot.joint_lower_limits)
     assert np.allclose(robot.action_space.high, robot.joint_upper_limits)
+
+    # Tests for IK. These values were found through the joint visualizer script.
+    # NOTE: we will probably update these values soon if we change the EE link.
+    retract_ee_pose = Pose(
+        position=(0.35817310214042664, 6.862916052341461e-06, 0.9498429298400879),
+        orientation=(
+            -0.00015761292888782918,
+            0.0087896678596735,
+            -0.009454049170017242,
+            0.999916672706604,
+        ),
+    )
+    raised_ee_pose = Pose(
+        position=(0.422219455242157, 0.0025123022496700287, 1.0768202543258667),
+        orientation=(
+            0.012197185307741165,
+            0.030934520065784454,
+            -0.14907421171665192,
+            0.9882667660713196,
+        ),
+    )
+    right_ee_pose = Pose(
+        position=(0.43226855993270874, -0.1964583396911621, 1.114035725593567),
+        orientation=(
+            0.04372112825512886,
+            0.1726122498512268,
+            -0.39378824830055237,
+            0.9017894864082336,
+        ),
+    )
+
+    for base_pose_offset in [
+        Pose((0, 0, 0)),
+        Pose((0, 1, 0)),
+        Pose.from_rpy((0, 0, 0), (np.pi / 2, 0, 0)),
+    ]:
+        robot.set_base(base_pose_offset)
+        for name, pose in [
+            ("retract", retract_ee_pose),
+            ("raised", raised_ee_pose),
+            ("right", right_ee_pose),
+        ]:
+            offset_target_pose = multiply_poses(base_pose_offset, pose)
+            joint_positions = inverse_kinematics(
+                robot, end_effector_pose=offset_target_pose, validate=True
+            )
+            robot.set_joints(joint_positions)
+            recovered_pose = robot.forward_kinematics(joint_positions)
+            # IK is analytic, so this should be extremely close.
+            assert np.allclose(
+                recovered_pose.position, offset_target_pose.position, atol=1e-6
+            ), f"IK failed for {name} with offset {base_pose_offset}"
